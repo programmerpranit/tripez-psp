7e4750cec8197cf90af961dbb93c82a7bdee5043
Pranit <pranit@webxstudio.in>
2023-04-22 22:47:14 +0530

    added design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,18 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a"}
-      }
+        primary: "#0070f3",
+        secondary: "#555555",
+        // skyblue: '#daf0ff',
+      },
     },
   },
   plugins: [],
-}
+};
