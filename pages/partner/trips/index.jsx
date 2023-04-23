8c73212a2732c2dd4316188aa871dc2ae6ffc7b0
Prathamesh Karambelkar <83802999+prathameshkarambelkar@users.noreply.github.com>
2023-04-24 00:09:51 +0530

    handle

--- pages/partner/trips/index.jsx
+++ pages/partner/trips/index.jsx
@@ -1,19 +1,16 @@
-import SearchCard from "@/components/SearchCard"
-import Trip from "@/components/Trip"
+import SearchCard from "@/components/SearchCard";
+import Trip from "@/components/Trip";
 
 export default function MyTrips() {
   return (
     <>
-    
       <h2 className="text-center my-5">My Trips</h2>
-<div className="max-w-3xl m-auto space-y-5">
-
-      <SearchCard/>
-      <SearchCard/>
-      <SearchCard/>
-      <SearchCard/>
-</div>
-    
+      <div className="max-w-3xl m-auto space-y-5">
+        <SearchCard />
+        <SearchCard />
+        <SearchCard />
+        <SearchCard />
+      </div>
     </>
-  )
+  );
 }
